2d83f09c1934310f2a05369bae02498599d844ec
Adam Bien <abien@adam-bien.com>
2017-05-22 09:12:24 +0200

    42 computation implemented

--- addition/src/main/java/com/airhacks/calculator/addition/boundary/Addition.java
+++ addition/src/main/java/com/airhacks/calculator/addition/boundary/Addition.java
@@ -1,12 +1,26 @@
 
 package com.airhacks.calculator.addition.boundary;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 
 @Stateless
 public class Addition {
 
     public int add(int a, int b) {
-        return a + b;
+        int result = a + b;
+        if (result == 42) {
+            thinkLonger();
+        }
+        return result;
+    }
+
+    void thinkLonger() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Addition.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
