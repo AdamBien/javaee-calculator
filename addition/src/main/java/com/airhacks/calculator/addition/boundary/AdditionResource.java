2cf8ec93bd2c49a790a32e2679123fb6f320a88c
Adam Bien <abien@adam-bien.com>
2017-05-22 03:31:54 +0200

    Timeout implemented

--- addition/src/main/java/com/airhacks/calculator/addition/boundary/AdditionResource.java
+++ addition/src/main/java/com/airhacks/calculator/addition/boundary/AdditionResource.java
@@ -1,11 +1,14 @@
 
 package com.airhacks.calculator.addition.boundary;
 
+import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 /**
  *
@@ -18,13 +21,15 @@ public class AdditionResource {
     Addition addition;
 
     @POST
-    public JsonObject addition(JsonObject input) {
+    public void addition(@Suspended AsyncResponse response, JsonObject input) {
+        response.setTimeout(1, TimeUnit.SECONDS);
         int a = input.getJsonNumber("a").intValue();
         int b = input.getJsonNumber("b").intValue();
         int result = addition.add(a, b);
-        return Json.createObjectBuilder().
+        JsonObject payload = Json.createObjectBuilder().
                 add("result", result).
                 build();
+        response.resume(payload);
     }
 
 }
