4a09b17d3966d462d1af43862a0f5710ff42217a
Adam Bien <abien@adam-bien.com>
2017-07-31 13:53:50 +0200

    100 requests with 10 Threads are hitting a 1sec blocking addition

--- addition/ab.sh
+++ addition/ab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ab -p src/test/resources/input.json -T"application/json" -c 10 -n 1000 http://localhost:8080/addition/resources/addition/
+ab -p src/test/resources/input.json -T"application/json" -c 10 -n 100 http://localhost:8080/addition/resources/addition/
